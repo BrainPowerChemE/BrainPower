4f4982b5e8824b8b1faa0928c32e4cd5f96b2eb8
Jay Werner <jmwerner@uw.edu>
2023-03-08 17:57:22 -0800

    update mrmr selector

--- brainpower/submodule/mrmr_feature_selection.py
+++ brainpower/submodule/mrmr_feature_selection.py
@@ -6,21 +6,37 @@ import sklearn.linear_model
 import seaborn as sns
 
 
-def mrmr_feature_selection(X_train, y_train,X_val,y_val,K,model):
+def mrmr_feature_selection(data_dev, split, 
+    min_features, max_features, step_features, folds,
+    tolerance=0.01,model=sklearn.linear_model.RidgeClassifier(),score=sklearn.metrics.balanced_accuracy_score()):
 
-    feature_performance = []
-    for k in K:
-        reduced_features = mrmr.mrmr_classif(X_train,y_train,K=k)
-        X_train_reduced = X_train[reduced_features]
-        X_val_reduced = X_val[reduced_features]
-        model.fit(X_train_reduced,y_train)
-        feature_performance.append([k,sklearn.metrics.balanced_accuracy_score(y_val,model.predict(X_val_reduced)),reduced_features])
-    #return feature_performance
+    """
+    data_dev: development data in a pandas dataframe
+    split: split proportion of training and val data each time a fold is executed
+    min_features: minimum number of features to return
+    max_features: maximum number of features to return
+    step_features: step between evaluation of features
+    folds: number of repeat evaluations of one feature space size. The training and validation data will be resampled from the dev data each time. It is important that test data is not fed to this function. 
+    tolerance: (Default=0.01) acceptable level of standard deviation between categories among the training and validation data. Percent categories are calculated first, then their stdev is calculated and compared to the stated tolerance
+
+
+    """
+
+    def mrmr_shorthand(X_train, y_train,X_val,y_val,K,model):
+        feature_performance = []
+        for k in K:
+            reduced_features = mrmr.mrmr_classif(X_train,y_train,K=k)
+            X_train_reduced = X_train[reduced_features]
+            X_val_reduced = X_val[reduced_features]
+            model.fit(X_train_reduced,y_train)
+            feature_performance.append([k,score(y_val,model.predict(X_val_reduced)),reduced_features])
+        return feature_performance
+
     folded_performances = []
     i=0
-    while i < 5:
+    while i < folds:
         # Split dev data
-        data_train, data_val, randstate = split_cats_by_tolerance(data_dev,0.01,silent=True)
+        data_train, data_val, randstate = split_cats_by_tolerance(data_dev,tolerance,silent=True,split=split)
 
         # Equalize the training data
         train_eq, randomstate = over_under(data_train,data_val)
@@ -30,7 +46,7 @@ def mrmr_feature_selection(X_train, y_train,X_val,y_val,K,model):
         y_traineq = train_eq['group']
         X_val = data_val.drop(columns='group')
         y_val = data_val['group']
-        feature_performances = mrmr_feature_selection(X_traineq,y_traineq,X_val,y_val,range(1,100),sklearn.linear_model.RidgeClassifier())
+        feature_performances = mrmr_shorthand(X_traineq,y_traineq,X_val,y_val,list(range(min_features,max_features,step_features)),model)
         folded_performances.append(feature_performances)
         i += 1
 
