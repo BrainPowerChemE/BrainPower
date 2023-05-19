868b2a90e64505417bba42cd1e2a276427fe8526
Katherine Liu <122061119+YuexuanL@users.noreply.github.com>
2023-05-18 21:10:26 -0700

    Update handle_scale_and_nan.py

--- brainpower/handle_scale_and_nan.py
+++ brainpower/handle_scale_and_nan.py
@@ -5,10 +5,10 @@ import sklearn
 def handle_scale_and_nan(df):
     features = list(df.select_dtypes(include='float64'))
     cat = list(df.select_dtypes(include='object'))
+    df = df.fillna(value=6)
     scaler = sklearn.preprocessing.StandardScaler().fit(df[features])
     df_cont = pd.DataFrame(data=scaler.transform(df[features]), columns=features)
     df_cat = pd.DataFrame(data=df[cat], columns=cat)
     df = pd.concat([df_cat,df_cont],axis=1)
-    df = df.fillna(value=10)
     
     return df
