61d7be646b87762807fb07e9203f45a201ca6a51
Haoxian <121738843+xsam0510@users.noreply.github.com>
2023-05-24 16:30:31 -0700

    Update __init__.py

--- brainpower/__init__.py
+++ brainpower/__init__.py
@@ -1,5 +1,5 @@
 from .handle_scale_and_nan import handle_scale_and_nan
 from .over_under import over_under
-from .make_confusion_matrix import make_confusion_matrix
+from .make_confusion_mtrx import make_confusion_mtrx
 from .select_features import select_features
 from .false_positives import false_positives
