2bcf89def0dd2fb2dd02e42ed35b8e5be2590c7e
Haoxian <121738843+xsam0510@users.noreply.github.com>
2023-05-24 18:43:04 -0700

    Update __init__.py

--- brainpower/__init__.py
+++ brainpower/__init__.py
@@ -2,4 +2,4 @@ from .handle_scale_and_nan import handle_scale_and_nan
 from .over_under import over_under
 from .make_confusion_mtrx import make_confusion_mtrx
 from .select_features import select_features
-from .false_positives import false_positives
+from .find_false_positive_patients import find_false_positive_patients
