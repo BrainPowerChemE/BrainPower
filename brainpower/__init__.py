9835f033ced50d4cc78b3d078219d9d14eddf562
mariam16548 <52047042+mariam16548@users.noreply.github.com>
2023-05-31 13:49:35 -0700

    Update __init__.py

--- brainpower/__init__.py
+++ brainpower/__init__.py
@@ -3,3 +3,5 @@ from .over_under import over_under
 from .make_confusion_mtrx import make_confusion_mtrx
 from .select_features import select_features
 from .find_false_positive_patients import find_false_positive_patients
+from .make_roc_curves import make_roc_curves
+
